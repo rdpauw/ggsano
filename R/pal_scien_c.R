ebe17ddee28519d2fd460eaebb5344e4a66cc221
Robby De Pauw <rdpauw@rshiny-intern.sciensano.be>
2021-03-26 14:44:52 +0100

    add new function to vizualize colours

---
+++ R/pal_scien_c.R
@@ -0,0 +1,18 @@
+#' Provide Continuous Sciensano Colours
+#'
+#' XXXXXXXXX
+#'
+#' @param low,high Colours for low and high ends of the gradient.
+#' @param space colour space in which to calculate gradient. Must be "Lab" - other values are deprecated.
+#' @param na.value Colour to use for missing values
+#' @param aesthetics Character string or vector of character strings listing the name(s) of the aesthetic(s) that this scale works with. This can be useful, for example, to apply colour settings to the colour and fill aesthetics at the same time, via aesthetics = c("colour", "fill").
+#' @return Character vector with #HEX colour codes
+#' @export
+pal_scien_c <- function (..., low = "#B2D235", high = "#006838", space = "Lab",
+          na.value = "grey50", guide = "colourbar", aesthetics = "colour") {
+                    continuous_scale(aesthetics, "gradient",
+                                     seq_gradient_pal(low, high, space),
+                                     na.value = na.value,
+                                     guide = guide, ...)
+}
+
--- R/pal_scien_d.R
+++ R/pal_scien_d.R
@@ -20,11 +20,11 @@ pal_scien_d <- function(n=13){
   sciensano2 <- "#006838"  # dark green
   sciensano3 <- "#B2D235"  # light green
   sciensano4 <- "#58595B"  # dark grey
-  sciensano5 <- "#7F3028"
-  sciensano6 <- "#284C7F"
-  sciensano7 <- "#284C7F"
-  sciensano8 <- "#6F287F"
-  sciensano9 <- "#B58939"
+  sciensano5 <- "#7F3028"  # dark red
+  sciensano6 <- "#284C7F"  # dark blue
+  sciensano7 <- "#284C7F"  # dark blue
+  sciensano8 <- "#6F287F"  # purple
+  sciensano9 <- "#B58939"  # beige
   sciensanoX <- "#709e54"  # Adrien's green for map points
   sciensano21 <- rgb(250, 213, 0, max = 255)  # yellow
   sciensano22 <- rgb(242, 157, 0, max = 255)  # orange
